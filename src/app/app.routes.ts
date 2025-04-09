9827956cfd81890f28bfef0b2b1aa6b36368ece8
JohanFR11 <johanfetecua11@gmail.com>
2025-04-09 17:36:35 -0500

    TaskManager

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { IniciosesionComponent } from './pages/iniciosesion/iniciosesion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'inicio', component: HomeComponent},
+    {path: 'registro', component: RegistroComponent},
+    {path: 'inicio_sesion', component: IniciosesionComponent},
+    {path: '**', redirectTo: 'inicio'}
+
+];
---
+++ src/app/pages/iniciosesion/iniciosesion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iniciosesion',
+  imports: [],
+  templateUrl: './iniciosesion.component.html',
+  styleUrl: './iniciosesion.component.scss'
+})
+export class IniciosesionComponent {
+
+}
